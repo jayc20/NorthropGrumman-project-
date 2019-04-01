b31118cdb649a56c6ac31e022934002901c5f165
jayc20 <jasenh2717@gmail.com>
2019-03-31 20:47:31 -0400

    Update MainActivity.js

--- MainActivity.js
+++ MainActivity.js
@@ -1,55 +1,102 @@
 import android.Manifest;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends Activity  {
+
+    //Bluetooth control
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+    // SPP UUID service
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    // MAC-address of Bluetooth module (you must edit this line)
+    private static String address = "98:D3:71:FD:51:47";
+
 
     //SMS message variables
     String etmessage = "Intruder has Entered the Base";
     //destination telephone number
     String etTelNr = "7543675392";
     int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+    int stop_value = 1;
 
     //Count down Variables
     private TextView CountdownText;
     private Button countdownbutton;
+    private Button Countdownstop;
 
     private CountDownTimer countDownTimer;
     private long timeLeftMillis = 120000;
     private boolean timeRunning;
+    BluetoothAdapter bluetoothAdapter;
+
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
 
+        Countdownstop = findViewById(R.id.Stop);
         CountdownText = findViewById(R.id.timertext);
         countdownbutton = findViewById(R.id.Start);
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBTState();
 
         countdownbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sendData("1");
+                Toast.makeText(getBaseContext(), "Alarm is On", Toast.LENGTH_SHORT).show();
                 startStop();
-
-                //Programming to send Sms message when start button is clicked
-                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) !=
-                        PackageManager.PERMISSION_GRANTED)
-                {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]
-                            {Manifest.permission.SEND_SMS},MY_PERMISSIONS_REQUEST_SEND_SMS);
+                if(stop_value > 0) {
+                    //Programming to send Sms message when start button is clicked
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) !=
+                            PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]
+                                {Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
+                    } else {
+                        SmsManager sms = SmsManager.getDefault();
+                        sms.sendTextMessage(etTelNr, null, etmessage, null, null);
+                        stop_value = stop_value - 1;
+                    }
                 }
-                else
+            }
+        });
+        Countdownstop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (timeRunning)
                 {
-                    SmsManager sms = SmsManager.getDefault();
-                    sms.sendTextMessage(etTelNr, null,etmessage,null,null);
+                    sendData("0");
+                    Toast.makeText(getBaseContext(), "Alarm is Off", Toast.LENGTH_SHORT).show();
+
+                    stopTimer();
+
                 }
             }
         });
@@ -65,20 +112,20 @@ public class MainActivity extends Activity {
     }
     public void startTimer()
     {
-      countDownTimer = new CountDownTimer(timeLeftMillis, 1000) {
-          @Override
-          public void onTick(long l) {
-              timeLeftMillis = l;
-              updateTimer();
-          }
+        countDownTimer = new CountDownTimer(timeLeftMillis, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeftMillis = l;
+                updateTimer();
+            }
 
-          @Override
-          public void onFinish() {
+            @Override
+            public void onFinish() {
 
-          }
-      }.start();
+            }
+        }.start();
 
-      timeRunning = true;
+        timeRunning = true;
     }
     public void stopTimer(){
         countDownTimer.cancel();
@@ -98,5 +145,122 @@ public class MainActivity extends Activity {
         timeLeftText += secondes;
         CountdownText.setText(timeLeftText);
 
+    }
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        if(Build.VERSION.SDK_INT >= 10){
+            try {
+                final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", new Class[] { UUID.class });
+                return (BluetoothSocket) m.invoke(device, MY_UUID);
+            } catch (Exception e) {
+                Log.e(TAG, "Could not create Insecure RFComm Connection",e);
+            }
+        }
+        return  device.createRfcommSocketToServiceRecord(MY_UUID);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "...onResume - try connect...");
+
+        // Set up a pointer to the remote node using it's address.
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        // Two things are needed to make a connection:
+        //   A MAC address, which we got above.
+        //   A Service ID or UUID.  In this case we are using the
+        //     UUID for SPP.
+
+        try {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e1) {
+            errorExit("Fatal Error", "In onResume() and socket create failed: " + e1.getMessage() + ".");
+        }
+
+        // Discovery is resource intensive.  Make sure it isn't going on
+        // when you attempt to connect and pass your message.
+        btAdapter.cancelDiscovery();
+
+        // Establish the connection.  This will block until it connects.
+        Log.d(TAG, "...Connecting...");
+        try {
+            btSocket.connect();
+            Log.d(TAG, "...Connection ok...");
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+            }
+        }
+
+        // Create a data stream so we can talk to server.
+        Log.d(TAG, "...Create Socket...");
+
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "...In onPause()...");
+
+        if (outStream != null) {
+            try {
+                outStream.flush();
+            } catch (IOException e) {
+                errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+            }
+        }
+
+        try     {
+            btSocket.close();
+        } catch (IOException e2) {
+            errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+        }
+    }
+
+    private void checkBTState() {
+        // Check for Bluetooth support and then check to make sure it is turned on
+        // Emulator doesn't support Bluetooth and will return null
+        if(btAdapter==null) {
+            errorExit("Fatal Error", "Bluetooth not support");
+        } else {
+            if (btAdapter.isEnabled()) {
+                Log.d(TAG, "...Bluetooth ON...");
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    private void errorExit(String title, String message){
+        Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    private void sendData(String message) {
+        byte[] msgBuffer = message.getBytes();
+
+        Log.d(TAG, "...Send data: " + message + "...");
+
+        try {
+            outStream.write(msgBuffer);
+        } catch (IOException e) {
+            String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+            if (address.equals("00:00:00:00:00:00"))
+                msg = msg + ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 35 in the java code";
+            msg = msg +  ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
+
+            errorExit("Fatal Error", msg);
+        }
     }
 }
